2dbf9d601ab238e22f8cd4af6d87e369083563f1
Alex C <cavaalex@tamu.edu>
2020-10-31 16:32:53 -0500

    Input for tag in Docker script

--- BuildAndPushDockerImg.sh
+++ BuildAndPushDockerImg.sh
@@ -1,4 +1,9 @@
 echo "Enter Docker Hub username"
 read username
-docker build -t $username/depicture:latest $(dirname $0)
-docker push $username/depicture:latest
+echo "Enter tag (blank for latest)"
+read tag
+if [ -z $tag ]; then
+    tag="latest"
+fi
+docker build -t $username/depicture:$tag $(dirname $0)
+docker push $username/depicture:$tag
