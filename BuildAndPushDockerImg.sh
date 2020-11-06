f3c071c36d50bce5d9498912b6aff90ebd73de55
Alex C <cavaalex@tamu.edu>
2020-11-05 19:59:50 -0600

    Better Docker script

--- BuildAndPushDockerImg.sh
+++ BuildAndPushDockerImg.sh
@@ -1,7 +1,12 @@
-echo "Enter Docker Hub username"
-read username
-echo "Enter tag (blank for latest)"
-read tag
+if [ -z $1 ]; then
+    echo "Enter Docker Hub username"
+    read username
+    echo "Enter tag (blank for latest)"
+    read tag
+else
+    username=$1
+    tag=$2
+fi
 if [ -z $tag ]; then
     tag="latest"
 fi
